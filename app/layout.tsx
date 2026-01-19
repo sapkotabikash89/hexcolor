f5e5edac6f01962157c8ea61a4ce3a42387d7bec
Bikash Sapkota <bikashsapkota@example.com>
2026-01-20 00:52:38 +0500

    Inject Grow by Mediavine globally in root layout for static HTML visibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,7 +94,22 @@ export default function RootLayout({
         <script
           data-grow-initializer=""
           dangerouslySetInnerHTML={{
-            __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");e.type="text/javascript";e.src="https://faves.grow.me/main.js";e.defer=true;e.setAttribute("data-grow-faves-site-id","U2l0ZTo5ZmZmYjE4Yi0wMmU2LTQ5YTYtYWRiYy05NGViMmU0OGU4NjY=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`,
+            __html: `!(function(){
+  window.growMe || (
+    (window.growMe = function(e){ window.growMe._.push(e); }),
+    (window.growMe._ = [])
+  );
+  var e = document.createElement("script");
+  e.type = "text/javascript";
+  e.src = "https://faves.grow.me/main.js";
+  e.defer = true;
+  e.setAttribute(
+    "data-grow-faves-site-id",
+    "U2l0ZTo5ZmZmYjE4Yi0wMmU2LTQ5YTYtYWRiYy05NGViMmU0OGU4NjY="
+  );
+  var t = document.getElementsByTagName("script")[0];
+  t.parentNode.insertBefore(e, t);
+})();`,
           }}
         />
         <WebsiteSchema />
