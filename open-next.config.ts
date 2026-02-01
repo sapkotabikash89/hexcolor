15c92b9859723c1cd8e529a94d3973277728c784
Bikash Sapkota <bikashsapkota@example.com>
2026-02-01 23:18:01 +0500

    chore: update open-next.config.ts per user changes

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,16 +1,15 @@
 import type { OpenNextConfig } from '@opennextjs/cloudflare';
 
 const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
+  override: {
+    wrapper: "cloudflare-node",
+    converter: "edge",
+    proxyExternalRequest: "fetch",
+    incrementalCache: "dummy",
+    tagCache: "dummy",
+    queue: "dummy",
   },
+  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
